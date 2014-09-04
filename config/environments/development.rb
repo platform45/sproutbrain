7fede4c33e1a3a81cd6520044d8efe12036f68c0
Katie <katiehahm@gmail.com>
2014-09-04 16:25:02 +0200

    emails production mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   #this is for mailcatcher only
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
   
 end
